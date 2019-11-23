4eb2b8a1a0e9c8c9dc9c405d43d47fc707801127
Guilherme Neves <up201306485@fe.up.pt>
2019-11-23 12:04:01 +0000

    "Unmirrored" the security camera

--- Proj2/MySecurityCamera.js
+++ Proj2/MySecurityCamera.js
@@ -4,7 +4,7 @@ class MySecurityCamera extends CGFobject {
         super(scene);
 
         this.rttText = rttText;
-        this.rectangle = new MyRectangle(this.scene, 'SecurityCamera', 1, 0.5, -0.5, -1);//0.5, 1, -0.5, -1);
+        this.rectangle = new MyRectangle2(this.scene, 'SecurityCamera', 0.5, 1, -0.5, -1);
         this.shader = new CGFshader(this.scene.gl, "shaders/shader.vert", "shaders/shader.frag");
         this.shader.setUniformsValues({ uSampler: 0, timeFactor: 0 });
     }
