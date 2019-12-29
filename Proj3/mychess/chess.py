cf1b7c26273c40d01ace1e6c4e8503b4aa6404e5
GNeves95 <up201306485@fe.up.pt>
2019-12-29 23:22:12 +0000

    Working on ai

--- Proj3/mychess/chess.py
+++ Proj3/mychess/chess.py
@@ -1,3 +1,5 @@
+#import chess
+
 class GameState:
     def __init__(self, board, movement):
         self.min = 99999
@@ -10,9 +12,58 @@ class GameState:
         auxChild = GameState(board,move)
         self.children.append(auxChild)
 
+def convertBoard(board):
+    boardArray = board.split('|')
+    i = 0
+    while i < len(boardArray):
+        if boardArray[i] == 'pw':
+            boardArray[i] = 'P'
+        elif boardArray[i] == 'rw':
+            boardArray[i] = 'R'
+        elif boardArray[i] == 'kw':
+            boardArray[i] = 'N'
+        elif boardArray[i] == 'bw':
+            boardArray[i] = 'B'
+        elif boardArray[i] == 'qw':
+            boardArray[i] = 'Q'
+        elif boardArray[i] == 'Kw':
+            boardArray[i] = 'K'
+        elif boardArray[i] == 'pb':
+            boardArray[i] = 'p'
+        elif boardArray[i] == 'rb':
+            boardArray[i] = 'r'
+        elif boardArray[i] == 'kb':
+            boardArray[i] = 'n'
+        elif boardArray[i] == 'bb':
+            boardArray[i] = 'b'
+        elif boardArray[i] == 'qb':
+            boardArray[i] = 'q'
+        elif boardArray[i] == 'Kb':
+            boardArray[i] = 'k'
+        else:
+            boardArray[i] = '.'
+        i += 1
+    return boardArray
+
+def getPossibleMoves(board):
+    moves = []
+    i = 0
+    while i < 8*8:
+        x = i%8
+        y = ((i-(i%8))/8)
+        orig = chr(ord('a')+(7-x))+str(8-y).split('.')[0]
+        print(orig + " - " + board[i])
+        if board[i] == 'P':
+            if y == 1:
+                moves.append('')
+
+        board[i]
+        i += 1
+
 def mainSolver(level, board, player):
     print(f'Solving')
-    boardArray = board.split('|')
+    boardArray = convertBoard(board)
+    getPossibleMoves(boardArray)
     #print(f'this is the boardArray {boardArray}')
     i = 7
     while i >= 0:
